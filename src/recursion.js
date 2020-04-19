b1a0bd3a0b8cb4569bac5bb703b76613a9b7ecc1
LDietrick <dietrick.larry@gmail.com>
2020-04-19 00:37:07 -0400

    Solve factorial, integer sum, and nested array sum

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,44 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  } else if (n === 0) {
+    return 1;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    var currentElement = array[0];
+    // if current element is an array, then call function on that element
+    if (Array.isArray(currentElement)) {
+      currentElement = arraySum(currentElement);
+    }
+    // sum is value of current element plus the sum of remaining ones
+    var result = currentElement + arraySum(array.slice(1));
+    return result;
+  }
 };
 
 // 4. Check if a number is even.
